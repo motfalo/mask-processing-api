5014f28237fab420cfa54e4e47aec932014455d6
Juliusz-Stefanski <59764731+Juliusz-Stefanski@users.noreply.github.com>
2020-06-03 10:36:43 +0200

    Update get_mean_and_low_peaks_value.py

--- get_mean_and_low_peaks_value.py
+++ get_mean_and_low_peaks_value.py
@@ -5,6 +5,9 @@ import os
 import numpy as np
 from datetime import time
 from scipy.signal import argrelextrema
+from scipy.interpolate import interp1d
+from scipy.signal import hilbert, chirp
+import re
 
 if __name__ == "__main__":
     files = fd.askopenfilenames()
@@ -31,7 +34,7 @@ if __name__ == "__main__":
 # =============================================================================
         
         
-        starting_time,ending_time=4800,6000
+        starting_time,ending_time=1000,3200
         probes_frame = probes[starting_time:ending_time]
         func=np.vectorize(lambda x:x/20)
         probes_frame=func(probes_frame)
@@ -53,31 +56,86 @@ if __name__ == "__main__":
 # =============================================================================
         
 
-
         values=np.array(values)
-        minimas=argrelextrema(values, np.less)
-        local_minimas=np.zeros(np.shape(values)[0])
 
-        for i in range (0,np.shape(minimas)[1]):
-            local_minimas[minimas[0][i]]=1
+
+        # plt.figure(i)
+        # plt.plot(probes_frame, values)
+        # plt.plot(probes_frame, local_minimas)
+        # plt.legend(["Wartoci surowe", "lokalne minima"])
+        # plt.xlabel("Time[s]")
+        # plt.ylabel("CO2")
+        # plt.title(f'{filename} \n mean value = {mean},\n local minimas = {min_peak_mean}')
+
+        # plt.show()
         
-        local_minimas = np.multiply(local_minimas, values)
-        mean = np.mean(values)
+        #amplitude envelope
+        analytic_signal = hilbert(values,None)
+        amplitude_envelope = np.abs(analytic_signal)
+        
+        
+        #getting minimas from amplitude envelope
+        minimas=argrelextrema(amplitude_envelope, np.less)
+        local_minimas=np.zeros(np.shape(amplitude_envelope)[0])
+
+        for j in range (0,np.shape(minimas)[1]):
+            local_minimas[minimas[0][j]]=1
+        
+        local_minimas = np.multiply(local_minimas, amplitude_envelope)
+        mean = np.mean(amplitude_envelope)
         local_minimas[local_minimas > mean] = 0
         number_of_low_peaks=0
         min_peak_mean=0
-        for i in range (0,np.shape(local_minimas)[0]):
-            if(local_minimas[i]!=0):
+        for j in range (0,np.shape(local_minimas)[0]):
+            if(local_minimas[j]!=0):
                 number_of_low_peaks+=1
-                min_peak_mean+=local_minimas[i]
+                min_peak_mean+=local_minimas[j]
         min_peak_mean/=number_of_low_peaks
-
-        plt.figure(i)
-        plt.plot(probes_frame, values)
-        plt.plot(probes_frame, local_minimas)
-        plt.legend(["Wartoci surowe", "lokalne minima"])
-        plt.xlabel("Time[s]")
-        plt.ylabel("CO2")
-        plt.title(f'{filename} \n mean value = {mean},\n local minimas = {min_peak_mean}')
-
-        plt.show()
+        
+        
+        #create and interpolate vector of values
+        interpolated_time=np.array([])
+        interpolated_minimas=np.array([])
+        for j in range (np.shape(local_minimas)[0]-1,-1,-1):
+            if(local_minimas[j]!=0):
+                interpolated_minimas=np.append(interpolated_minimas,local_minimas[j])
+                interpolated_time=np.append(interpolated_time,[j])
+                
+        interpolated_time=np.flip(interpolated_time)
+        interpolated_minimas=np.flip(interpolated_minimas)
+        
+        interpolation = interp1d(interpolated_time, interpolated_minimas,kind='cubic',bounds_error=False)
+        time_correction_interpolation=np.vectorize(lambda x:x/20+starting_time/20)
+        interpolated_time_corrected=time_correction_interpolation(interpolated_time)
+        #,bounds_error=False
+# =============================================================================
+#         newx = np.zeros(1200)
+#         for i in range (1,1201):
+#             newx[i-1]=i
+#         
+# =============================================================================
+        newx = np.linspace(0,ending_time-starting_time,num=ending_time-starting_time)
+        
+        newy = interpolation(newx)
+# =============================================================================
+#         plt.plot(newx, newy)
+#         plt.scatter(interpolated_time,interpolated_minimas, s=10)
+#         plt.show()
+# =============================================================================
+        
+        fig = plt.figure(i)
+        ax0 = fig.add_subplot(211)
+        ax0.plot(probes_frame, values, label='signal')
+        ax0.plot(probes_frame, amplitude_envelope, label='envelope')
+        ax0.plot(probes_frame, newy, label='local_minimas')
+        ax0.set_xlabel("time in seconds")
+        ax0.set_ylabel("CO2")
+        ax0.scatter(interpolated_time_corrected,interpolated_minimas, s=10)
+        ax0.legend(["Wartosci surowe", "obwiednia", "krzywa wdechu"])
+        ax0.set_title(f'{filename} \n mean value = {mean},\n local minimas = {min_peak_mean}')
+        ax1 = fig.add_subplot(212)
+        ax1.plot(probes_frame, values, label='signal')
+        ax1.set_xlabel("time in seconds")
+        ax1.legend(["Wartosci surowe", "krzywa wdechu"])
+        ax1.plot(probes_frame, newy, label='local_minimas')
+        
